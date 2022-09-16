d96c50d85f990ce31f46e8fdbe7749186446f062
bhavi-p <96447354+bhavi-p@users.noreply.github.com>
2022-09-16 18:25:58 -0400

    Revert "base: test-branch"

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/HeaderPic_nobg.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h3>Hello I'm</h3>
+        <h1>Bhavi</h1>
+        <h4 className="text-light">Full-Stack Developer</h4>
+
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll_down'> Scroll Down to Connect ></a>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
